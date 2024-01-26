73c394347350e1c182a2fc87be74674a5f0b7dfc
refatbaderkhan <refatbaderkhan@gmail.com>
2024-01-26 15:45:36 +0200

    added MusicWindowAlbumsCarousel component

--- src/@/components/ui/MusicWindow.jsx
+++ src/@/components/ui/MusicWindow.jsx
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import MusicWindowGenreFilter from './MusicWindowGenreFilter'
-import MusicWindowAlbumCard from './MusicWindowAlbumCard'
+import MusicWindowAlbumsCarousel from './MusicWindowAlbumsCarousel'
 import { spotifyNewAlbumReleases } from 'api/spotify/spotifyServices'
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel"
 
 const MusicWindow = () => {
   
@@ -18,22 +17,7 @@ useEffect(() => {
   return (
     <div className='MusicWindow h-82% pl-10 pr-10 pt-3.5 pb-3'>
       <MusicWindowGenreFilter />
-      <div className='MusicWindowTitle text-xl font-semibold text-[#BFBFBF] mt-3.5'>
-      New Releases
-      </div>
-      <div className="mt-3.5">
-      <Carousel>
-        <CarouselContent className="pl-5">
-          {albums.map((album) => (
-            <CarouselItem key={album.id} className="basis-1/5">
-              <MusicWindowAlbumCard album={album} />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious className="border border-0 bg-[#515151] text-[#BFBFBF] hover:bg-[#FFDE5A]"/>
-        <CarouselNext className="border border-0  bg-[#515151] text-[#BFBFBF] hover:bg-[#FFDE5A]"/>
-      </Carousel>
-      </div>
+      <MusicWindowAlbumsCarousel carouselTitle='New Releases' albums={albums}/>
     </div>
   )
 }
---
+++ src/@/components/ui/MusicWindowAlbumsCarousel.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel"
+import MusicWindowAlbumCard from './MusicWindowAlbumCard'
+
+
+
+const MusicWindowAlbumsCarousel = ({carouselTitle, albums}) => {
+  return (
+    <div>
+    <div className='MusicWindowTitle text-xl font-semibold text-[#BFBFBF] mt-3.5'>
+    {carouselTitle}
+    </div>
+    <div className="mt-3.5">
+    <Carousel>
+      <CarouselContent className="">
+        {albums.map((album) => (
+          <CarouselItem key={album.id} className="basis-1/5">
+            <MusicWindowAlbumCard album={album} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="border border-0 bg-[#515151] text-[#BFBFBF] hover:bg-[#FFDE5A]"/>
+      <CarouselNext className="border border-0  bg-[#515151] text-[#BFBFBF] hover:bg-[#FFDE5A]"/>
+    </Carousel>
+    </div>
+    </div>
+  )
+}
+
+export default MusicWindowAlbumsCarousel
